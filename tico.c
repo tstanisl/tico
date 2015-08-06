786154595c4e7e2fb746b10fccb2729b5c2df4ee
Tomasz Stanislawski <stanislawski.tomasz@gmail.com>
2015-08-06 17:16:03 +0200

    tico: remove unused code

--- tico.c
+++ tico.c
@@ -8,11 +8,6 @@
 
 int main()
 {
-#if 0
-	for (int i = 0; i < n_positions; ++i)
-		if (positions[i].n_children <= 0)
-			dump_position(&positions[i]);
-#endif
 	struct player_fo *ai_perfect = ai_perfect_init();
 	if (!ai_perfect) {
 		fprintf(stderr, "ai_perfect_init() failed\n");
